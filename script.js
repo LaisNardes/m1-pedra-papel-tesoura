f119d06c7cadb0032b2860341e573e921b9e2509
LaisNardes <lais.nardes0398@gmail.com>
2024-05-08 20:32:30 -0400

    Projeto Completo

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
 /* Desenvolva seu código abaixo */
+/*"Jogador 1 venceu!", "Jogador 2 venceu!" ou "Empate!". */
+function playRockPaperScissor(player1, player2){
+    let resultado = "";
+
+    if (player1 == "Papel" && player2 == "Pedra"){
+        resultado = "Jogador 1 venceu!";
+
+    } else if (player1 == "Papel" && player2 == "Tesoura"){
+       resultado = "Jogador 2 venceu!";
+
+    } else if (player1 == "Papel" && player2 == "Papel"){
+        resultado = "Empate!";
+
+    } else if (player1 == "Tesoura" && player2 == "Pedra"){
+        resultado = "Jogador 2 venceu!";
+
+    } else if (player1 == "Tesoura" && player2 == "Tesoura"){
+        resultado = "Empate!";
+
+    } else if (player1 == "Tesoura" && player2 == "Papel"){
+        resultado = "Jogador 1 venceu!";
+
+    } else if (player1 == "Pedra" && player2 == "Papel"){
+        resultado = "Jogador 2 venceu!";
+
+    } else if (player1 == "Pedra" && player2 == "Tesoura"){
+        resultado = "Jogador 1 venceu!";
+
+    } else {
+        resultado = "Empate!";
+    }
+
+      return resultado;   
+
+} 
+
+console.log(playRockPaperScissor("Tesoura","Tesoura"));
